903c30dfcc97a7045028f814a4834e86f3725bd5
Pooya Hekmati <s.pooyahekmati.a@gmail.com>
2024-01-26 17:27:11 -0500

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -12,17 +12,18 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'Music Embedding'
-copyright = '2021, SeyyedPooya HekmatiAthar'
-author = 'Pooya Hekmati'
+project = "Music Embedding"
+copyright = "2021-24, SeyyedPooya HekmatiAthar"
+author = "Pooya Hekmati"
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.7 alpha'
+release = "1.1.0"
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,17 +31,18 @@ release = '0.1.7 alpha'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
-    'numpydoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.doctest',
-    'sphinx.ext.inheritance_diagram']
+    "sphinx.ext.autodoc",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+    "numpydoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.doctest",
+    "sphinx.ext.inheritance_diagram",
+]
 autosummary_generate = True
 autosummary_imported_members = True
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -54,27 +56,27 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 
-html_theme_path=[r'C:\Users\Dynamic\AppData\Roaming\Python\Python39\site-packages']
+html_theme_path = [r"C:\Users\Dynamic\AppData\Roaming\Python\Python39\site-packages"]
 html_theme = "sphinx_rtd_theme"
-html_style = 'css/style.css'
-html_logo = '_static/img/music_embedding.jpg'
-html_favicon = '_static/img/favicon.png'
+html_style = "css/style.css"
+html_logo = "_static/img/music_embedding.jpg"
+html_favicon = "_static/img/favicon.png"
 html_theme_options = {
-    'logo_only': True,
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'vcs_pageview_mode': '',
-    'style_nav_header_background': '#222',
+    "logo_only": True,
+    "display_version": True,
+    "prev_next_buttons_location": "bottom",
+    "style_external_links": False,
+    "vcs_pageview_mode": "",
+    "style_nav_header_background": "#222",
     # Toc options
-    'collapse_navigation': False,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
+    "collapse_navigation": False,
+    "sticky_navigation": True,
+    "navigation_depth": 4,
+    "includehidden": True,
+    "titles_only": False,
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
